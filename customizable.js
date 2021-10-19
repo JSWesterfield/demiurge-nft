7c9d854076e90ae46592e131d673cce49fe3de8c
Jake Westerfield <Jacob_Westerfield@discovery.com>
2021-10-18 23:50:28 -0400

    created customizable js for customizing existing avatars created

---
+++ customizable.js
@@ -0,0 +1,5 @@
+const { createAvatar } = require('@dicebear/avatars');
+const style = require('@dicebear/open-peeps');
+// const style = require('@dicebear/adventurer'); // optional secondary
+const Rarepress = require('rarepress');
+
