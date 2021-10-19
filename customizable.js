92f8df1034ea0339cb6d72dca18e36f8b0cb79a2
Jake Westerfield <Jacob_Westerfield@discovery.com>
2021-10-18 23:52:37 -0400

    adds in declarations for index and total to 15

--- customizable.js
+++ customizable.js
@@ -3,3 +3,11 @@ const style = require('@dicebear/open-peeps');
 // const style = require('@dicebear/adventurer'); // optional secondary
 const Rarepress = require('rarepress');
 
+// Simply increment the INDEX value to mint collections from a new address every time.
+const INDEX = 1; 
+// Tweak the TOTAL attribute to change how many items you want to mint in your collection.
+const TOTAL = 15;
+
+(async () => {
+    
+})
