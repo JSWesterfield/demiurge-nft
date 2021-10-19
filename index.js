49f5b19c65af3b34ad7f4355d93cbd8b4117ac57
Jake Westerfield <Jacob_Westerfield@discovery.com>
2021-10-18 23:32:05 -0400

    adds in comment for tokenizing the svg and avatar image

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const Rarepress = require('rarepress');
     const rarepress = new Rarepress();
     await rarepress.init({ network: "mainnet" })
     for(let i= 0; i < 1000; i++ ) {
+        // image to tokenize is the svg or created Avatar 
         let svg = createAvatar(style, {seed: i.toString() });
+        // 
         let cid = await rarepress.fs.add(Buffer.from(svg))
+        // tokenize the image
         let token = await rarepress.token.createAvatar({
             metadata: {
                 name: `${1}`,
